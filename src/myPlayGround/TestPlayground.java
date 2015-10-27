3a8f243409a49577a135ef46fa8a7b07ca420164
CYF <da_tou@126.com>
2015-10-27 09:01:29 +0800

    small adjustment

--- src/myPlayGround/TestPlayground.java
+++ src/myPlayGround/TestPlayground.java
@@ -1,7 +1,11 @@
 package myPlayGround;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestPlayground {
 	public static void main(String[] args){
-		
+		List<Integer> list = null;
+		System.out.println(list.size());
 	}
 }
