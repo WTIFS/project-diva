963480e98c366191e10db7b142a40d4ea8a2b0a6
CYF <da_tou@126.com>
2015-12-31 14:53:03 +0800

    remove ProjectNodeJS

--- src/myNodeJS/learnyounode/program.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Yuanfei on 2015/12/28.
- */
-console.log("HELLO WORLD");
---
+++ src/myPlayGround/TestStr.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Yuanfei on 2015/12/30.
+ */
+var str1 = "";
+var str2 = "1";
+var str3 = "1,";
+var str4 = "1,2";
+var str5 = ",";
+
+console.log(str1.split(','));
+console.log(str2.split(','));
+console.log(str3.split(','));
+console.log(str4.split(','));
+console.log(str5.split(','));
+
+
