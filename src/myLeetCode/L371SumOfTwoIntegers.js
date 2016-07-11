8257d8c7c659e4f327727c7bbe6446398c8e8d35
chenyuanfei <chenyuanfei@91jinrong.com>
2016-07-11 16:06:41 +0800

    Add 371. Sum of Two Integers

---
+++ src/myLeetCode/L371SumOfTwoIntegers.js
@@ -0,0 +1,30 @@
+/**
+ * Created by chenyuanfei on 16/7/11.
+ */
+/*
+371. Sum of Two Integers  QuestionEditorial
+Total Accepted: 10602
+Total Submissions: 20444
+Difficulty: Easy
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+    Given a = 1 and b = 2, return 3.
+
+*/
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while (b!=0) {
+        var carrier = (a & b) * 2;
+        a = (a ^= b);
+        b = carrier;
+    }
+    return a;
+};
+
+console.log(getSum(-10, 2));
