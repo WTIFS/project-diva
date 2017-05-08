9168fad7d84146f1c81311968a0c63bc3178f866
CYF <da_tou@126.com>
2017-05-08 17:30:13 +0800

    add myQueue

---
+++ src/myLib/myQueue.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Yuanfei on 2017/5/8.
+ */
+
+var MAX_CAPACITY = 16;
+
+var Queue = function(capacity) {
+    if (!capacity) capacity = MAX_CAPACITY;
+    if (capacity > MAX_CAPACITY) capacity = MAX_CAPACITY;
+    this.capacity = capacity;
+    this.nums = new Array(capacity);
+    this.begin = 0;
+    this.end = -1;
+    this.cnt = 0;
+};
+
+Queue.prototype.push = function(num) {
+    if (this.cnt==this.capacity) {
+        console.log("OVERFLOW");
+        return;
+    }
+    this.end = (this.end+1) % this.capacity;
+    this.nums[this.end] = (num);
+    this.cnt++;
+    console.log(this);
+};
+
+Queue.prototype.pull = function() {
+    if (this.cnt==0) {
+        console.log("EMPTY QUEUE");
+        return;
+    }
+    var result = this.nums[this.begin];
+    this.begin = (this.begin+1) % this.capacity;
+    this.cnt--;
+    console.log(result);
+    return result;
+};
+
+var queue = new Queue(2);
+queue.push(1);
+queue.pull();
+queue.push(2);
+queue.pull();
+queue.push(3);
+queue.push(4);
+queue.pull();
+queue.push(5);
