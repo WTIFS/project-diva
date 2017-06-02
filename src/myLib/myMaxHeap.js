c0a45ad208fd5617dbaf63aa9c6aa324b43ef8a3
CYF <da_tou@126.com>
2017-06-02 18:51:36 +0800

    add my impletation of heap

---
+++ src/myLib/myMaxHeap.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Yuanfei on 2017/6/2.
+ * Max Heap
+ */
+
+var MaxHeap = function() {
+    this.nums = [];
+};
+
+
+MaxHeap.prototype.push = function(num) {
+    var nums = this.nums;
+    nums.push(num);
+    var i = nums.length - 1;
+    while (i>0) {
+        var r = parseInt((i-1) / 2);
+        if (nums[i]>nums[r]) {
+            swap(nums, i, r);
+            i = r;
+        } else break;
+    }
+};
+
+
+MaxHeap.prototype.pop = function() {
+    var nums = this.nums;
+    var n = nums.length;
+    swap(nums, 0, n-1);
+    nums.pop();
+    n--;
+    var r = 0;
+    var largestIndex = 0;
+    while (r<=parseInt(n-1)/2) {
+        var left = 2*r + 1;
+        var right = left + 1;
+        if (left<n && nums[left]>nums[largestIndex]) largestIndex = left;
+        if (right<n && nums[right]>nums[largestIndex]) largestIndex = right;
+        if (largestIndex != i) {
+            swap(nums, i, largestIndex);
+            i = largestIndex;
+        } else break;
+    }
+
+};
+
+
+MaxHeap.prototype.init = function() {
+    var nums = this.nums;
+    var n = nums.length;
+    for (var r = parseInt((n-1)/2); r>=0; r--) { //root = last parent -> 0
+        var i = r;
+        var largestIndex = i;
+        while (i<n) {
+            var left = 2*i + 1;
+            var right = left + 1;
+            if (left<n && nums[left]>nums[largestIndex]) largestIndex = left;
+            if (right<n && nums[right]>nums[largestIndex]) largestIndex = right;
+            if (largestIndex != i) {
+                swap(nums, i, largestIndex);
+                i = largestIndex;
+            } else break;
+        }
+    }
+};
+
+
+MaxHeap.prototype.setNums = function(args) {
+    this.nums = args;
+};
+
+var swap = function(nums, a, b) {
+    var tmp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = tmp;
+};
+
+module.exports = new MaxHeap();
+
+
