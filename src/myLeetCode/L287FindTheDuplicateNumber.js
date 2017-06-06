156949efb75f07ae7c3a2fa40c5171fd9a5c87fa
CYF <da_tou@126.com>
2017-06-06 16:44:02 +0800

    add 287

--- src/myLeetCode/L287FindTheDuplicateNumber.js
+++ src/myLeetCode/L287FindTheDuplicateNumber.js
@@ -10,6 +10,9 @@
  Your runtime complexity should be less than O(n2).
  There is only one duplicate number in the array, but it could be repeated more than once.
 
+ See 142 Linked List Cycle
+ nums长度为1+n，值为1 to n，值可视作指针，则问题变为找到环路的起点
+
  *
  */
 
@@ -19,5 +22,20 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-
+    var slow = nums[0];
+    var fast = nums[nums[0]];
+    while (slow!=fast) {
+        slow = nums[slow]; //slow = slow.next
+        fast = nums[nums[fast]]; //fast = fast.next.next
+    }
+    slow = 0;
+    while (slow!=fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
 };
+
+console.log(findDuplicate([1, 2, 3, 4, 5, 1]));
+console.log(findDuplicate([1, 2, 3, 4, 5, 2]));
+console.log(findDuplicate([1, 1]));
