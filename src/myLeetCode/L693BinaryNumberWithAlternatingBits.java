6ce8cd1cb12bf3c7a553480662be5da67172d69d
WTIFS <dadadadadatou@126.com>
2018-03-26 23:57:59 +0800

    add 693. Binary Number with Alternating Bits

---
+++ src/myLeetCode/L693BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,41 @@
+package myLeetCode;
+
+/**
+ * Created by WTIFS-Mac on 2018/3/26.
+
+ Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+ Example 1:
+ Input: 5
+ Output: True
+ Explanation:
+ The binary representation of 5 is: 101
+ Example 2:
+ Input: 7
+ Output: False
+ Explanation:
+ The binary representation of 7 is: 111.
+ Example 3:
+ Input: 11
+ Output: False
+ Explanation:
+ The binary representation of 11 is: 1011.
+ Example 4:
+ Input: 10
+ Output: True
+ Explanation:
+ The binary representation of 10 is: 1010.
+
+ */
+public class L693BinaryNumberWithAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        int last = -1;
+        while (n>0) {
+            int current = n & 1;
+            if (last != current) last = current;
+            else return false;
+            n >>= 1;
+        }
+        return true;
+    }
+}
