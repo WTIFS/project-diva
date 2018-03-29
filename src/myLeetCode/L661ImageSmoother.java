a5efbdbccf0d1c849e577c440f0a5c23db462b56
WTIFS <dadadadadatou@126.com>
2018-03-30 01:05:50 +0800

    debug

--- src/myLeetCode/L661ImageSmoother.java
+++ src/myLeetCode/L661ImageSmoother.java
@@ -26,25 +26,40 @@ package myLeetCode;
  */
 public class L661ImageSmoother {
 
-    public int[][] imageSmoother(int[][] M) {
+    public static int[][] imageSmoother(int[][] M) {
         if (M.length==0 || M[0].length==0) return M;
         int height = M.length;
         int width = M[0].length;
-        int[][] result = M.clone();
+        System.out.println(height + " " + width);
+        int[][] result = new int[height][width];
         for (int i=0; i<height; i++) {
             for (int j=0; j<width; j++) {
                 int sum = 0;
+                int count = 0;
                 for (int offset1=-1; offset1<=1; offset1++) {
                     for (int offset2=-1; offset2<=1; offset2++) {
                         if (i+offset1>=0 && i+offset1<height && j+offset2>=0 && j+offset2<width) {
                             sum += M[i+offset1][j+offset2];
+                            count++;
                         }
                     }
                 }
-                result[i][j] = sum;
+                System.out.println(i + ", " + j + " " + sum + " " + count);
+                result[i][j] = sum/count;
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] m = new int[][]{
+                new int[]{2,3,4},
+                new int[]{5,6,7},
+                new int[]{8,9,10},
+                new int[]{11,12,13},
+                new int[]{14,15,16}
+        };
+        System.out.println(imageSmoother(m)[4][2]);
     }
 
 }
