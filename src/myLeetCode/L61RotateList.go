02956ebdc1223f76893a6369a087690c328a4718
chenyuanfei <da_tou@126.com>
2021-02-23 21:16:48 +0800

    add 61 Rotate List

---
+++ src/myLeetCode/L61RotateList.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	h := &ListNode{Val: 1}
+	fmt.Printf("%+v\n\n", L64RotateList(h, 1))
+
+	h = &ListNode{Val: 1, Next: &ListNode{Val: 2}}
+	fmt.Printf("%+v\n\n", L64RotateList(h, 1))
+
+	h = &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2}}}
+	fmt.Printf("%+v\n", L64RotateList(h, 1))
+
+	h = &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2}}}
+	fmt.Printf("%+v\n", L64RotateList(h, 2))
+
+	h = &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2}}}
+	fmt.Printf("%+v\n", L64RotateList(h, 3))
+
+	h = &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2}}}
+	fmt.Printf("%+v\n\n", L64RotateList(h, 4))
+}
+
+func L64RotateList(head *ListNode, k int) *ListNode {
+	if head == nil || head.Next == nil || k == 0 {
+		return head
+	}
+
+	//先循环遍历1次，获取长度
+	var tail *ListNode
+	var l int
+	for i := head; i != nil; i = i.Next {
+		tail = i
+		l++
+	}
+	tail.Next = head
+
+	last := head
+	k = l - (k % l)
+	for i := 0; i < k-1; i++ {
+		last = last.Next
+	}
+	head = last.Next
+	last.Next = nil
+	return head
+}
