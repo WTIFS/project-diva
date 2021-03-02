1ae968750ee14f8aff63c3bcf1660420b6327c8c
WTIFS <da_tou@126.com>
2021-03-02 23:01:10 +0800

    update slice examples

--- src/myGo/complex_type/slice.go
+++ src/myGo/complex_type/slice.go
@@ -5,9 +5,9 @@ import "fmt"
 /***
 slice实际上的结构是：
 struct {
-	data *ptr //指向数组中某一元素的指针
-	len  int  //切片长度
-	cap  int  //切片容量
+	data unsafe.Pointer //指向数组中某一元素的指针
+	len  int            //切片长度
+	cap  int            //切片容量
 }
 
 因此两个引用同一底层数组的切片，其中一个发生元素修改，可能会影响另一个切片
@@ -31,23 +31,34 @@ func main() {
 	//a: [1 0], b: [1], c: [], d: [0]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 
-	//这次 b 的 append 连带修改了a、d
+	//这次 b 的 append 连带修改了a、c、d
 	c = append(b, 2)
+	//可以看到a、b、c的指针都是一样的，d的指针 = a[0] + int占用的长度(8字节)
+	//aptr: 0xc000016070, bptr: 0xc000016070, cptr: 0xc000016070, dptr: 0xc000016078
+	fmt.Printf("aptr: %v, bptr: %v, cptr: %v, dptr: %v\n", &a[0], &b[0], &c[0], &d[0])
 	//a: [1 2], b: [1], c: [1 2], d: [2]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 
-	//a、c、d的值都被改动了
+	//同上
 	_ = append(b, 3)
 	//a: [1 3], b: [1], c: [1 3], d: [3]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 
+	//因此会有这种线下：x里附加的明明是2，y里附加3，结果却是x里也是2
+	x := append(b, 2)
+	y := append(b, 3)
+	//a: [1 3], b: [1], c: [1 3], d: [3], x: [1 3], y: [1 3]
+	fmt.Printf("a: %v, b: %v, c: %v, d: %v, x: %v, y: %v\n", a, b, c, d, x, y)
+
 	//c此时发生了扩容，等号右侧的返回值换了底层数组
 	c = append(c, 4)
+	//cap(c)=4，容量翻倍
+	//fmt.Println(cap(c))
 	//a: [1 3], b: [1], c: [1 3 4], d: [3]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 	//现在再操作c不会影响a、b了
 	c[0] = 0
-	//a: [1 3], b: [1], c: [0 3 4]
+	//a: [1 3], b: [1], c: [0 3 4], d: [3]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 
 	//在函数里也一样，如果未发生扩容，会连带修改a
@@ -55,6 +66,7 @@ func main() {
 	appendToIntSlice(b, 6)
 	//a: [1 6], b: [1], c: [0 3 4], d: [6]
 	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
+
 }
 
 func appendToIntSlice(a []int, num int) {
