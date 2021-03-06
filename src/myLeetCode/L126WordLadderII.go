c2585cce9ae86c19cf8ec2c35cb27608d7f6eb3b
WTIFS <da_tou@126.com>
2021-03-07 01:31:54 +0800

    add 126. Word Ladder II

--- src/myLeetCode/L126WordLadderII.go
+++ src/myLeetCode/L126WordLadderII.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 /***
@@ -38,51 +37,75 @@ All the strings in wordList are unique.
 */
 
 func main() {
-	//res := findLadders("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"})
-	//fmt.Printf("%v\n", res)
-
-	res := findLadders("red", "tax", []string{"ted", "tex", "red", "tax", "tad", "den", "rex", "pee"})
+	res := findLadders("a", "c", []string{"a", "b", "c"})
 	fmt.Printf("%v\n", res)
 
+	res = findLadders("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"})
+	fmt.Printf("%v\n", res)
+
+	res = findLadders("red", "tax", []string{"ted", "tex", "red", "tax", "tad", "den", "rex", "pee"})
+	fmt.Printf("%v\n", res)
+
+	res = findLadders("hot", "dog", []string{"hot", "dog"})
+	fmt.Printf("%v\n", res)
+
+	res = findLadders("cet", "ism",
+		[]string{"kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"})
+	fmt.Printf("%v\n", res)
 }
 
 func findLadders(beginWord string, endWord string, wordList []string) [][]string {
 	res := make([][]string, 0)
-	toVisit := []string{beginWord}
-	steps := []int{0}
-	minStep := math.MaxInt32
-	wordMap := StringList2BoolMap(wordList)
+	dict := StringList2BoolMap(wordList)
 	paths := [][]string{{beginWord}}
-	if !wordMap[endWord] {
-		return res
-	}
+	end := false
 
-	for visitIndex := 0; visitIndex < len(toVisit); visitIndex++ {
-		if steps[visitIndex] > minStep {
-			return res
-		}
-		visitWord := toVisit[visitIndex]
-		visitRune := []rune(visitWord)
-		for i := range visitRune {
-			c := visitRune[i]
-			for j := byte(0); j < 26; j++ {
-				visitRune[i] = rune('a' + j)
-				if string(visitRune) == endWord {
-					minStep = steps[visitIndex]
-					p := make([]string, len(paths[visitIndex]))
-					copy(p, paths[visitIndex])
-					p = append(p, endWord)
-					paths = append(paths, p)
-					res = append(res, p)
-				} else if wordMap[string(visitRune)] {
-					toVisit = append(toVisit, string(visitRune))
-					steps = append(steps, steps[visitIndex]+1)
-					p := make([]string, len(paths[visitIndex]))
-					copy(p, paths[visitIndex])
-					paths = append(paths, append(p, string(visitRune)))
+	//遍历toVisit队列
+	toVisit := []string{beginWord}
+	for len(toVisit) > 0 && !end {
+		l := len(toVisit)
+
+		//每次遍历一层
+		visited := make(map[string]bool)
+		for i := 0; i < l; i++ {
+			//本步使用的单词
+			curWord := toVisit[i]
+			curRune := []rune(curWord)
+
+			//遍历该单词的每一位
+			for j := range curRune {
+				c := curRune[j]
+				//修改每一位字母
+				for k := byte(0); k < 26; k++ {
+					curRune[j] = rune('a' + k)
+					newWord := string(curRune)
+					if newWord == curWord || !dict[newWord] {
+						continue
+					}
+					visited[newWord] = true
+
+					if newWord == endWord {
+						end = true
+						path := appendWordToPath(paths[i], endWord)
+						res = append(res, path)
+						//如果旧单词距离 + 1 > 新单词，说明之前有更短的路径到新单词，则本次不用旧单词 + 1
+					} else {
+						toVisit = append(toVisit, newWord)
+						path := appendWordToPath(paths[i], newWord)
+						paths = append(paths, path)
+					}
 				}
+				curRune[j] = c
 			}
-			visitRune[i] = c
+		}
+
+		//弹出本层单词
+		toVisit = toVisit[l:]
+		paths = paths[l:]
+
+		//将本层单词从字典中移除，避免后续走重复路径
+		for k := range visited {
+			dict[k] = false
 		}
 	}
 
@@ -95,4 +118,11 @@ func StringList2BoolMap(list []string) map[string]bool {
 		m[value] = true
 	}
 	return m
+}
+
+func appendWordToPath(paths []string, word string) []string {
+	p := make([]string, len(paths)+1)
+	copy(p, paths)
+	p[len(paths)] = word
+	return p
 }
